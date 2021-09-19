f2b2ff762d534ddec9ab028616f1609f211ee2ac
Carlos <carlos.dom13@hotmail.com>
2021-09-19 03:15:39 +0200

    paginacion de alumnos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material/paginator'
 import { AppComponent } from './app.component';
 import { AlumnosComponent } from './components/alumnos/alumnos.component';
 import { CursosComponent } from './components/cursos/cursos.component';
 import { ExamenesComponent } from './components/examenes/examenes.component';
 import { AlumnosFormComponent } from './components/alumnos/alumnos-form/alumnos-form.component';
 import { LayoutModule } from './layout/layout.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { LayoutModule } from './layout/layout.module';
     AppRoutingModule,
     LayoutModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/alumnos/alumnos.component.ts
+++ src/app/components/alumnos/alumnos.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import Swal from 'sweetalert2';
 import { Alumno } from 'src/app/models/alumno';
 import { AlumnoService } from 'src/app/services/alumno.service';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-alumnos',
@@ -11,11 +12,26 @@ import { AlumnoService } from 'src/app/services/alumno.service';
 export class AlumnosComponent implements OnInit {
   titulo = 'Listado de alumnos'
   alumnos: Alumno[];
-  constructor(private service: AlumnoService) { }
+  totalRegistros = 0;
+  paginaActual = 0;
+  totalPorPagina = 4;
+  pageSizeOptions = [3, 5, 10, 25, 100];
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor(private service: AlumnoService) { }
   ngOnInit(): void {
-    this.service.listar().subscribe(alumnos =>{
-      this.alumnos = alumnos;
+    this.calcularRango();
+  }
+  paginar(event: PageEvent): void{
+    this.paginaActual = event.pageIndex;
+    this.totalPorPagina = event.pageSize;
+    this.calcularRango();
+  }
+  private calcularRango(){
+    this.service.listarPaginas(this.paginaActual.toString(), this.totalPorPagina.toString()).subscribe(r =>{
+      this.alumnos = r.content as Alumno[];
+      this.totalRegistros = r.totalElements as number;
+      this.paginator._intl.itemsPerPageLabel = 'Registros por pagina:';
     });
   }
   public eliminar(alumno: Alumno): void{
@@ -31,7 +47,8 @@ export class AlumnosComponent implements OnInit {
       if(res.value){
         if(confirm(`¿Seguro que desea eliminar a ${alumno.id}?`)){
           this.service.eliminar(alumno.id).subscribe(()=>{   
-            this.alumnos = this.alumnos.filter(a=>a!==alumno);
+            // this.alumnos = this.alumnos.filter(a=>a!==alumno);
+            this.calcularRango();
             Swal.fire('Registro borrado', `Alumno ${alumno.nombre} eliminado con exito`, 'success');
           });
         }
